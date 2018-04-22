7487033555e716e70bca80555e46b30a707e0d33
Raghav Batra <rb698@cornell.edu>
2018-04-22 01:03:11 -0400

    Add files via upload

--- frontend/src/components/charts/metadata_chart.jsx
+++ frontend/src/components/charts/metadata_chart.jsx
@@ -26,110 +26,147 @@ export class MetadataChart extends React.PureComponent {
     const categoryColors = ["#03353e", "#0294a5"];
 
     const len = Object.keys(jsonMod).length;
-    const svgWidth = 500;
-    const svgHeight = 500;
+    const svgWidth = 1000;
+    const svgHeight = 1000;
     const svg = d3.select(node)
-      .attr("width", svgWidth)
-      .attr("height", svgHeight)
-      .attr("class", "bar");
-    const maxOffset = 0.9 * 400; // 360
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("class", "bar");
+
+
+    const maxOffset = 0.9 * 600;
+
+    // scale
     const yScale = d3.scaleLinear()
-      .domain([0, 1])
-      .range([0, maxOffset]);
-    let pushX = 120;
-    const pushY = 60;
-    const interBarSpace = 60;
-    const barWidth = 20;
-    let ctr = 0;
-    let flag = 0;
-    let bars;
+    .domain([0, 1])
+    // percentage between 0 and 1
+    .range([0, maxOffset]);
+
+    const yScaleCopy = d3.scaleLinear()
+    .domain([0, 1])
+    // percentage between 0 and 1
+    .range([maxOffset, 0]);
+
+    // var pushX = 120;
+    // var pushY = 60;
+
+    const interBarSpace = 100;
+    const barWidth = 40;
+
+    const ctr = 0;
+
+    const flag = 0;
+    const bars;
 
     // plot bars
-    for (var ind in jsonMod) {
-        var jsonData = jsonMod[ind];
-        var entries = Object.entries(jsonData);
+
+    for (let ind in jsonMod) {
+
+        // console.log(ind);
+        const jsonData = jsonMod[ind];
+
+        const entries = Object.entries(jsonData);
+        console.log(entries);
+
         if (flag == 0) {
-            var yMax = d3.max(entries, (d, i) => { return d[1]; });
+            var yMax = d3.max(entries, function (d, i) { return d[1]; });
             flag++;
         }
 
         // data join
         bars = svg.selectAll("#rect" + ind)
-          .data(entries)
-          .enter();
+        .data(entries)
+        .enter();
 
-        const div = d3.select(".metadata-chart").append("div")
-          .attr("class", "tooltip")
-          .style("opacity", 0);
+        var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
         // tool tip
         bars.append("rect")
-          .attr("width", barWidth)
-          .attr("height", (d, i) => {return yScale(entries[i][1]); })
-          .attr("x",  (d, i) => { return pushX + interBarSpace * i; })
-          .attr("y", (d, i) => { return pushY + yScale(yMax) - yScale(entries[i][1]); })
-          .attr("fill", categoryColors[ctr])
-          .on("mouseover", function(d) {
-            const rect = d3.select(this)._groups[0][0]
-            const rectX = Number(rect.attributes["x"].nodeValue);
-            const rectY = Number(rect.attributes["y"].nodeValue);
+        .attr("width", barWidth)
+        .attr("height", function(d, i) {return yScale(entries[i][1]); })
+        .attr("x", function (d, i) { return pushX + interBarSpace * i; })
+        .attr("y", function (d, i) { return pushY + yScale(yMax) - yScale(entries[i][1]); })
+        .attr("fill", categoryColors[ctr])
+        .on("mouseover", function(d) {
+            let rect = d3.select(this)._groups[0][0]
+            let rectX = Number(rect.attributes["x"].nodeValue) + 10;
+            let rectY = Number(rect.attributes["y"].nodeValue);
             div.transition()
-              .duration(200)
-              .style("opacity", .9);
+            .duration(200)
+            .style("opacity", .9);
             div.html(Math.round(Number(d[1] * 100) * 100) / 100 + "%")
-              .style("left", rectX + "px")
-              .style("top", rectY - 10 + "px");
-          })
-          .on("mouseout", d => {
-            div.transition()
-              .duration(500)
-              .style("opacity", 0);
-          });
+            .style("left", rectX + "px")
+            .style("top", rectY - 10 + "px");
+        })
+        .on("mouseout", function(d) {
+        div.transition()
+        .duration(500)
+        .style("opacity", 0);
+        });
 
         pushX += barWidth;
+
         ctr++;
+
     };
 
     // x-axis titles
-    pushX = 120;
+    pushX = 270;
 
     bars
-      .append("text")
-      .text((d, i) => { return entries[i][0]; } )
-      .attr("x",(d, i) => { return pushX + interBarSpace * i; })
-      .attr("y",(d, i) => { return pushY - 5 + yScale(yMax) + 20; })
-      .attr("fill", "black")
-      .style("font-size", 14);
+    .append("text")
+    .text( function(d, i) { return entries[i][0]; } )
+    .attr("x", function(d, i) { return pushX + interBarSpace * i; })
+    .attr("y", function(d, i) { return pushY - 5 + yScale(yMax) + 20; })
+    .attr("fill", "black")
+    .style("font-size", 14);
 
     // legend
-    const pushTextX = 120;
-    const pushTextY = 180;
-    const legendBarSize = 10;
 
-    categories.forEach((d, ind) => {
-      bars
+    var pushTextX = 270;
+    var pushTextY = 300;
+
+    var legendBarSize = 20;
+
+    var yAxis = d3.axisRight(yScaleCopy);
+
+    svg.append('g')
+    .attr("id", "g_x")
+    .attr('transform', 'translate(' + (pushX - 75) + ', 120) scale(0.75, 0.75)')
+    .call(yAxis);
+
+
+    categories.forEach( function(d, ind) {
+
+
+        bars
         .append("text")
-        .text( d => { return categories[ind]; } )
-        .attr("x", d => { return pushTextX + interBarSpace * (entries.length + 1); })
-        .attr("y", d => { return pushTextY + 20 * ind; })
+        .text( function(d) { return categories[ind]; } )
+        .attr("x", function(d) { return pushTextX + interBarSpace * (entries.length + 1); })
+        .attr("y", function(d) { return pushTextY + 25 * ind; })
         .attr("fill", "black")
-        .style("font-size", 14);
+        .style("font-size", 18);
 
-      bars
+
+        bars
         .append("rect")
         .attr("width", legendBarSize)
         .attr("height", legendBarSize)
-        .attr("x", d => { return pushTextX + interBarSpace * (entries.length + 1) - 15; })
-        .attr("y", d => { return pushTextY + 20 * ind - 10; })
-        .attr("fill", d => {return categoryColors[ind]; })
-        .style("font-size", 14);
-    });
+        .attr("x", function(d) { return pushTextX + interBarSpace * (entries.length + 1) - 25; })
+        .attr("y", function(d) { return pushTextY + 25 * ind - 15; })
+        .attr("fill", function(d) {return categoryColors[ind]; })
+        .style("font-size", 18);
+
+    } );
+
   }
 
   render() {
     return (
       <div className="metadata-chart">
-        <svg ref={node => this.node = node} />
+        <svg ref={node => this.node = node} width={1000} height={1000} />
       </div>
     );
   }
--- frontend/src/components/charts/screen_time_chart.jsx
+++ frontend/src/components/charts/screen_time_chart.jsx
@@ -35,102 +35,132 @@ export class ScreenTimeChart extends React.PureComponent {
     // length is 2
     const len = Object.keys(jsonMod).length;
 
-    const svgWidth = 500;
-    const svgHeight = 500;
+    const svgWidth = 1000;
+    const svgHeight = 1000;
 
     const svg = d3.select(node)
     .attr("width", svgWidth)
     .attr("height", svgHeight)
     .attr("class", "bar");
 
-    const maxOffset = 0.9 * 400; // 360
-    const yScale = d3.scaleLinear().domain([0, 1]).range([0, maxOffset]);
-    let pushX = 120;
-    const pushY = 180;
-    const interBarSpace = 30;
-    const barWidth = 20;
-    let ctr = 0;
-    const flag = 0;
+    var maxOffset = 0.9 * 600;
+
+    // scale
+    var yScale = d3.scaleLinear()
+    .domain([0, 1])
+    // percentage between 0 and 1
+    .range([0, maxOffset]);
+
+    var yScaleCopy = d3.scaleLinear()
+    .domain([0, 1])
+    // percentage between 0 and 1
+    .range([maxOffset, 0]);
+
+    // var pushX = 120;
+    // var pushY = 180;
+
+    var interBarSpace = 60;
+    var barWidth = 40;
+
+    var ctr = 0;
+
+    var flag = 0;
+    var bars;
 
     // plot bars
-    const yMax = d3.max(entries, (d, i) => { return d[1]; });
 
-    // data join
-    let bars = svg.selectAll("#rect")
-      .data(entries)
-      .enter();
+      var yMax = d3.max(entries, function (d, i) { return d[1]; });
 
-    const div = d3.select(".screen-time-chart").append("div")
-      .attr("class", "tooltip")
-      .style("opacity", 0);
+        // data join
+        bars = svg.selectAll("#rect")
+        .data(entries)
+        .enter();
 
-    // tool tip
-    bars.append("rect")
-      .attr("width", barWidth)
-      .attr("height", (d, i) => {return yScale(entries[i][1]); })
-      .attr("x", (d, i) => { return pushX + interBarSpace * i; })
-      .attr("y", (d, i) => { return pushY + yScale(yMax) - yScale(entries[i][1]); })
-      .attr("fill", categoryColors[ctr])
-      .on("mouseover", function(d) {
-          var rect = d3.select(this)._groups[0][0]
-          var rectX = Number(rect.attributes["x"].nodeValue);
-          var rectY = Number(rect.attributes["y"].nodeValue);
-          div.transition()
+        var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+        // tool tip
+        bars.append("rect")
+        .attr("width", barWidth)
+        .attr("height", function(d, i) {return yScale(entries[i][1]); })
+        .attr("x", function (d, i) { return pushX + interBarSpace * i; })
+        .attr("y", function (d, i) { return pushY + yScale(yMax) - yScale(entries[i][1]); })
+        .attr("fill", categoryColors[ctr])
+        .on("mouseover", function(d) {
+            let rect = d3.select(this)._groups[0][0]
+            let rectX = Number(rect.attributes["x"].nodeValue) + 10;
+            let rectY = Number(rect.attributes["y"].nodeValue);
+            div.transition()
             .duration(200)
             .style("opacity", .9);
-          div.html(Math.round(Number(d[1] * 100) * 100) / 100 + "%")
+            div.html(Math.round(Number(d[1] * 100) * 100) / 100 + "%")
             .style("left", rectX + "px")
             .style("top", rectY - 10 + "px");
-      })
-      .on("mouseout", d => {
+        })
+        .on("mouseout", function(d) {
         div.transition()
-          .duration(500)
-          .style("opacity", 0);
-      });
+        .duration(500)
+        .style("opacity", 0);
+        });
 
-    pushX += barWidth;
-    ctr++;
+        pushX += barWidth;
+
+        ctr++;
 
     // x-axis titles
-    pushX = 120;
 
     bars
-      .append("text")
-      .text( (d, i) => { return entries[i][0]; } )
-      .attr("x", (d, i) => { return pushX + interBarSpace * i + 15; })
-      .attr("y", (d, i) => { return pushY + yScale(yMax) - yScale(entries[i][1]) - 5; })
-      .attr("transform", (d, i) => {
-          var x = pushX + interBarSpace * i + 15;
-          var y = pushY + yScale(yMax) - yScale(entries[i][1]) - 5;
-          return "rotate(-90," + x + "," + y + ")";
-      })
-      .attr("fill", "black")
-      .style("font-size", 14);
+    .append("text")
+    .text( function(d, i) { return entries[i][0]; } )
+    .attr("x", function(d, i) { return pushX + interBarSpace * i - 20; })
+    .attr("y", function(d, i) { return pushY + yScale(yMax) - yScale(entries[i][1]) - 10; })
+    .attr("transform", function(d, i) {
+        let x = pushX + interBarSpace * i - 20;
+        let y = pushY + yScale(yMax) - yScale(entries[i][1]) - 10;
+
+        return "rotate(-90," + x + "," + y + ")";
+    })
+    .attr("fill", "black")
+    .style("font-size", 14);
 
     // legend
-    const pushTextX = 120;
-    const pushTextY = 180;
-    const legendBarSize = 10;
 
-    categories.forEach((d, ind) => {
-      bars
+    var pushTextX = pushX;
+    var pushTextY = pushY;
+
+    var legendBarSize = 20;
+
+    var yAxis = d3.axisRight(yScaleCopy);
+
+    svg.append('g')
+    .attr("id", "g_x")
+    .attr('transform', 'translate(' + (pushX - 75) + ', 40) scale(0.75, 0.75)')
+    .call(yAxis);
+
+    categories.forEach( function(d, ind) {
+
+        bars
         .append("text")
-        .text( d => { return categories[ind]; } )
-        .attr("x", d => { return pushTextX + interBarSpace * (entries.length + 1); })
-        .attr("y", d => { return pushTextY + 20 * ind; })
+        .text( function(d) { return categories[ind]; } )
+        .attr("x", function(d) { return pushTextX + interBarSpace * (entries.length + 1) - 20; })
+        .attr("y", function(d) { return pushTextY + 25 * ind; })
         .attr("fill", "black")
-        .style("font-size", 14);
+        .style("font-size", 18);
 
-      bars
+
+        bars
         .append("rect")
         .attr("width", legendBarSize)
         .attr("height", legendBarSize)
-        .attr("x", d => { return pushTextX + interBarSpace * (entries.length + 1) - 15; })
-        .attr("y", d => { return pushTextY + 20 * ind - 10; })
-        .attr("fill", d => {return categoryColors[ind]; })
-        .style("font-size", 14);
-    });
-  }
+        .attr("x", function(d) { return pushTextX + interBarSpace * (entries.length + 1) - 45; })
+        .attr("y", function(d) { return pushTextY + 25 * ind - 15; })
+        .attr("fill", function(d) {return categoryColors[ind]; })
+        .style("font-size", 18);
+
+      });
+
+}
 
   render() {
     return (
