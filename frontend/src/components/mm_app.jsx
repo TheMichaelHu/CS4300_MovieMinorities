284cc95a77f103215300c033bf7a2bed558c80e7
Michael Hu <michaelanhu@gmail.com>
2018-04-30 16:09:19 -0400

    :sparkles:

--- frontend/src/components/mm_app.jsx
+++ frontend/src/components/mm_app.jsx
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import {
-  lightGreen200, lightGreen500,
+  yellow200, yellow500,
   cyan700,
   grey50, grey400, grey800, grey900,
 } from 'material-ui/styles/colors';
@@ -94,9 +94,9 @@ export class _MovieMinoritiesApp extends React.Component {
         primary1Color:      grey900,
         primary2Color:      grey800,
         primary3Color:      grey400,
-        accent1Color:       lightGreen500,
+        accent1Color:       yellow500,
         accent2Color:       cyan700,
-        accent3Color:       lightGreen200,
+        accent3Color:       yellow200,
         textColor:          grey50,
         secondaryTextColor: fade(grey50, 0.7),
         alternateTextColor: grey50,
