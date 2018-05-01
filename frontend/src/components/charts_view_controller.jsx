e8cb62ccd635bb99134f4ee94cc41e4035d36216
levakabas <lakabas15@gmail.com>
2018-04-30 21:32:21 -0400

    fingers crossed

--- frontend/src/components/charts_view_controller.jsx
+++ frontend/src/components/charts_view_controller.jsx
@@ -4,6 +4,7 @@ import { MetadataChart } from "./charts/metadata_chart";
 import { ScreenTimeChart } from "./charts/screen_time_chart";
 import { RaceChart } from "./charts/race_chart";
 import { BechdelChart } from "./charts/bechdel";
+import { EmpathGenderChart } from "./charts/empath_gender";
 
 // import '../styles/logo';
 
@@ -12,19 +13,27 @@ export class ChartsVc extends React.PureComponent {
     return (
     <div className="contain">
       <div className="charts-wrapper row">
-        <div className="col-xs-12 col-md-6">
+        <div className="col-xs-12 col-md-5">
           <MetadataChart movie={this.props.movie} />
         </div>
-        <div className="col-xs-12 col-md-6">
+        <div className="col-xs-12 col-md-7">
           <ScreenTimeChart movie={this.props.movie} />
         </div>
       </div>
       <div className="charts-wrapper row">
-        <div className="col-xs-12 col-md-6">
+        <div className="col-xs-12 col-md-5">
           <RaceChart movie={this.props.movie} />
         </div>
-        <div className="col-xs-12 col-md-6">
+        <div className="col-xs-12 col-md-7">
           <BechdelChart movie={this.props.movie} />
+        </div>
+      </div>
+      <div className="charts-wrapper row">
+        <div className="col-xs-12 col-md-5">
+          <EmpathGenderChart movie={this.props.movie} />
+        </div>
+        <div className="col-xs-12 col-md-7">
+          <EmpathGenderChart movie={this.props.movie} />
         </div>
       </div>
     </div>
@@ -38,6 +47,8 @@ export class ChartsVc extends React.PureComponent {
         <ScreenTimeChart movie={this.props.movie} />
         <RaceChart movie={this.props.movie} />
         <BechdelChart movie={this.props.movie} />
+        <EmpathGenderChart movie={this.props.movie} />
+        <EmpathGenderChart movie={this.props.movie} />
       </div>
     );
   }
